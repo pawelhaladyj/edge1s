f855643d5b26d9712cf2d86d5a31755f23135cc2
Paweł <pawel@haladyj.pl>
2020-02-07 16:50:07 +0100

    initial commit

---
+++ src/main/java/pl/haladyj/pawelhaladyjservice/PawelHaladyjServiceApplication.java
@@ -0,0 +1,13 @@
+package pl.haladyj.pawelhaladyjservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PawelHaladyjServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PawelHaladyjServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/haladyj/pawelhaladyjservice/PawelHaladyjServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.haladyj.pawelhaladyjservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PawelHaladyjServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
