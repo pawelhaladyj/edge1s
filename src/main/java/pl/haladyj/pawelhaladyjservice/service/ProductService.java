f68df6201063676342a22fe02bb13a682d975d1d
Paweł <pawel@haladyj.pl>
2020-02-16 19:42:08 +0100

    removing comments

--- src/main/java/pl/haladyj/pawelhaladyjservice/service/ProductService.java
+++ src/main/java/pl/haladyj/pawelhaladyjservice/service/ProductService.java
@@ -1,10 +1,8 @@
 package pl.haladyj.pawelhaladyjservice.service;
 
-import pl.haladyj.pawelhaladyjservice.model.Product;
 import pl.haladyj.pawelhaladyjservice.service.dto.ProductCounterDto;
 import pl.haladyj.pawelhaladyjservice.service.dto.ProductDto;
 
-import java.util.List;
 import java.util.Optional;
 
 public interface ProductService {
